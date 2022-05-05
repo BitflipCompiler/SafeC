30248ef07df34f0070c670ec46e2c1a26872d824
JeBenzon <jonathaneisbenzon@gmail.com>
2022-05-05 15:41:13 +0200

    output folder

---
+++ output/output.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+float age = 19;
+char * name = "KarlEmil";
+bool binary = true;
+char  favChar = 'n';
+float newAge;
+bool x;
+float setAge(float agen){
+age = agen;
+age = 10 + 10;
+return age;
+}
+bool isOver18(){
+bool is18 = false;
+
+if (age > 18){
+is18 = true;
+}
+
+return is18;
+}
+void main(){
+newAge = setAge(20);
+x = isOver18();
+
+}
